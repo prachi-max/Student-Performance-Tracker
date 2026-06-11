b5b862efd466ec8639934dd0a01b4bf35d6f60b7
prachi-max <prachi.nawale09@gmail.com>
2026-06-12 00:53:02 +0530

    Prepare update

--- Backend/server.js
+++ Backend/server.js
@@ -22,6 +22,9 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
+app.get("/", (req, res) => {
+  res.send("Student Performance Tracker API is running 🚀");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
